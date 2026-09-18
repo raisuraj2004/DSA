e8ed133fccc861ad421a31b1df4ac4fea07826d4
SURYA RAI <168733481+raisuraj2004@users.noreply.github.com>
2026-09-19 00:32:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.01%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,6 +7,14 @@ class Solution:
         for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
-            
+        
+        # or we can do with this method as well (without array,Bottom-up)
+        # a = 1
+        # b = 1
+
+        # for i in range(2, n + 1):
+        #     a, b = b, a + b
+
+        # return b
 
         
